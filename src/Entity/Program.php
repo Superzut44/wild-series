9208c8c8ce67c778cf61a7fca4c9d7667f0a1a80
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-12-02 11:54:58 +0100

    06 - Symfony : Créer ta première entité avec Doctrine

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -22,6 +22,16 @@ class Program
      */
     private $title;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $summary;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $poster;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +45,30 @@ class Program
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
 
         return $this;
     }
